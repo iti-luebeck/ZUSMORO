efb041fb02f90b946b761a6b952c23200794b896
Ida Massow <ida110189@aol.com>
2012-06-09 13:34:27 +0200

    Comments and deletions in MainFrame

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -15,52 +15,99 @@ import model.AbstractRobot;
 import model.Automat;
 import model.onboard.OnBoardEpuck;
 
+/**
+ * @author ida
+ *
+ * Main class starting Zusmoro, generating main window with standard layout
+ */
+//removed logger, maybe put back in?
+//TODO: was kann privat werden?
+
+
 public class MainFrame extends JFrame {
 
-	// public static Logger logger = Logger.getLogger("view.MainFrameLogger");
+	/* view */
+	/**
+	 * Main window holding all other elements
+	 */
 	public static final MainFrame mainFrame = new MainFrame();
-	public static final float VERSION = 1.1f;
-	public static boolean DEBUG = false;
+	/**
+	 * Text field at bottom of mainFrame for user information, e.g. transmission
+	 * status, mode, actions on mouse over etc.
+	 */
 	public static StatusBar statusBar;
+	/**
+	 * Panel holding buttons for user actions, e.g. adding components to state
+	 * chart, start debugging etc.
+	 */
 	public static ToolPanel toolPanel;
+	/**
+	 * Menu bar at top of mainFrame for saving, loading etc.
+	 */
 	public static MenuBar menuBar;
+	/**
+	 * The editor panel holding the state chart in progress
+	 */
 	public volatile static EditorPanel editorPanel;
+
+	/* model */
+	/**
+	 * The automat in the editor window
+	 */
 	public volatile static Automat automat;
+	/**
+	 * define which kind of robot is used
+	 */
 	public volatile static Class<AbstractRobot> robotClass;
 
+	// TODO: should be control?
+	/**
+	 * Interaction with EPuck: start & stop program, debug
+	 */
 	public volatile static OnBoardEpuck onBoard;
-	// protected boolean fileIsInSync = false;
+
+	/**
+	 * Version of ZUSMORO
+	 */
+	public static final float VERSION = 1.1f;
+	/**
+	 * debug mode on?
+	 */
+	public static boolean DEBUG = false;
 
 	private static final long serialVersionUID = -5093064061027429275L;
 	Container cPane;
 	private JScrollPane scrollPane;
 
+	/**
+	 * Constructor generating mainFrame
+	 */
 	@SuppressWarnings("unchecked")
 	public MainFrame() {
 		super();
 		try {
-			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+			UIManager
+					.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
 		} catch (Exception e) {
 			e.printStackTrace();
-			// MainFrame.logger.log(Level.CONFIG,
-			// "Loading Nimbus Look and Feel failed.", e);
 			try {
-				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+				UIManager.setLookAndFeel(UIManager
+						.getSystemLookAndFeelClassName());
 			} catch (Exception e2) {
 				e2.printStackTrace();
-				// MainFrame.logger.log(Level.CONFIG,
-				// "Loading System Look and Feel failed.", e);
 			}
 		}
 		try {
-			MainFrame.robotClass = (Class<AbstractRobot>) Class.forName("robots.epuck.EPuckRobot");
+			MainFrame.robotClass = (Class<AbstractRobot>) Class
+					.forName("robots.epuck.EPuckRobot");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			// robotClass = EPuckRobot.class;
 		}
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setTitle("ZUSMORO - ZUstandbasierte Steuerung für MObile ROboter v" + VERSION);
+		setTitle("ZUSMORO - ZUstandbasierte Steuerung für MObile ROboter v"
+				+ VERSION);
 		setSize(1000, 700);
 		MainFrame.menuBar = new MenuBar();
 		setJMenuBar(MainFrame.menuBar);
@@ -79,39 +126,30 @@ public class MainFrame extends JFrame {
 
 	}
 
+	/**
+	 * @param args
+	 *            default
+	 * @throws Exception
+	 * 
+	 *             main method starting ZUSMORO
+	 */
 	public static void main(String[] args) throws Exception {
 		CommPortFinder.listPorts();
-		// logger.setLevel(Level.ALL);
-		// logger.addHandler(new FileHandler("logfile.txt"));
-		// logger.entering("MainFrame", "main", args);
-		// mainFrame = new MainFrame();
 		mainFrame.setVisible(true);
 		ToolTipManager.sharedInstance().setInitialDelay(400);
-		// SwingUtilities.invokeLater(new Runnable() {
-		// public void run() {
-		// new TransitionView().setVisible(true);
-		// }
-		// });
-		((Graphics2D) mainFrame.getGraphics()).setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+		((Graphics2D) mainFrame.getGraphics()).setRenderingHint(
+				RenderingHints.KEY_TEXT_ANTIALIASING,
 				RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
-		// logger.exiting("MainFrame", "main");
-	}
-
-	public void setEditorPanel(EditorPanel ePanel) {
-		cPane.remove(scrollPane);
-		MainFrame.editorPanel = ePanel;
-		scrollPane = new JScrollPane(MainFrame.editorPanel);
-		cPane.add(scrollPane, BorderLayout.CENTER);
-		validate();
-		repaint();
 	}
 
 	@Override
 	public void dispose() {
 		if (MainFrame.automat.getStateCount() > 0) {
-			int confirm = JOptionPane.showConfirmDialog(MainFrame.mainFrame,
-					"<html>Soll das bestehende Programm<br>wirklich verworfen werden?</html>", "Programm schließen",
-					JOptionPane.YES_NO_OPTION);
+			int confirm = JOptionPane
+					.showConfirmDialog(
+							MainFrame.mainFrame,
+							"<html>Soll das bestehende Programm<br>wirklich verworfen werden?</html>",
+							"Programm schließen", JOptionPane.YES_NO_OPTION);
 			if (confirm == JOptionPane.YES_OPTION) {
 				System.exit(0);
 			}
